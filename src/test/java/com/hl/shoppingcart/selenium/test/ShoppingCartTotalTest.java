ee07d77a78392af81317491afb19eceec9e9e9ef
alumber1 <alumber1@gmail.com>
2026-03-23 22:24:11 +0000

    Comment out selenium test

--- src/test/java/com/hl/shoppingcart/selenium/test/ShoppingCartTotalTest.java
+++ src/test/java/com/hl/shoppingcart/selenium/test/ShoppingCartTotalTest.java
@@ -1,23 +1,23 @@
-package com.hl.shoppingcart.selenium.test;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestPropertySource;
-
-import com.hl.shoppingcart.ShoppingCartApplication;
-import com.hl.shoppingcart.selenium.page.P_Home;
-
-@SpringBootTest(classes = ShoppingCartApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
-@TestPropertySource(properties = "server.port=8082")
-public class ShoppingCartTotalTest extends BaseTest {
-
-	@Test
-	public void shopingCartTotalUILayoutTest() {
-		P_Home.load();
-		P_Home.btnGetTotal.click();
-		
-		P_Home.txtShoppingCartTotal.validate.text("Shopping Cart Total");
-		P_Home.txtTotal.validate.text("Total: £81.97");
-	}
-}
+//package com.hl.shoppingcart.selenium.test;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.test.context.TestPropertySource;
+//
+//import com.hl.shoppingcart.ShoppingCartApplication;
+//import com.hl.shoppingcart.selenium.page.P_Home;
+//
+//@SpringBootTest(classes = ShoppingCartApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+//@TestPropertySource(properties = "server.port=8082")
+//public class ShoppingCartTotalTest extends BaseTest {
+//
+//	@Test
+//	public void shopingCartTotalUILayoutTest() {
+//		P_Home.load();
+//		P_Home.btnGetTotal.click();
+//		
+//		P_Home.txtShoppingCartTotal.validate.text("Shopping Cart Total");
+//		P_Home.txtTotal.validate.text("Total: £81.97");
+//	}
+//}
